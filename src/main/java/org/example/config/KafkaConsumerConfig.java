9c3f76da864e385abb33b3c94aaa27298cafbeaf
dunice <n.samsonov@tg.dunice.net>
2023-01-26 17:43:04 +0300

    KAFKA_ADVERTISED_HOST_NAME: ${HOST_IP} хуй знает что это , не работает консюмер из за этого

--- src/main/java/org/example/config/KafkaConsumerConfig.java
+++ src/main/java/org/example/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.example.util.AppConstant;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -18,12 +19,15 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers")
+    private String boostrapServers;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                AppConstant.bootstrapService);
+                AppConstant.bootstrapServer);
         props.put(
                 ConsumerConfig.GROUP_ID_CONFIG,
                 AppConstant.groupId);
--- src/main/java/org/example/util/AppConstant.java
+++ src/main/java/org/example/util/AppConstant.java
@@ -1,8 +1,11 @@
 package org.example.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class AppConstant {
 
     public static String groupId= "nodejs-consumer";
     public static String topicName = "myserver.public.todo";
-    public static String bootstrapService = "localhost:9092";
+
+    public static String bootstrapServer = "localhost:9092";
 }
